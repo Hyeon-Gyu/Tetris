ad708c9c885e147c7cc0957c3221bac30c1b86b9
hyeon-gyu <hyungue729@naver.com>
2023-06-27 13:02:38 +0900

    useeffect logic revised

--- tetris/src/App.tsx
+++ tetris/src/App.tsx
@@ -98,6 +98,7 @@ function App() {
             console.log("myboard undefined");
             return;
         }
+        console.log("내 보드 처음 꺼내왓을때 상태 :",myboard.state)
         if (userkey == 'q') {
             myboard.state = TetrisState.Finished
             setDrawScreen(myboard!!.oScreen.get_array()) // 렌더링 코드
@@ -109,7 +110,7 @@ function App() {
             stompClient!.send("/app/chat.send", {}, JSON.stringify(chatMessageQuit))
         }
         console.log("here")
-        myboard.state = myboard.accept(userkey)
+        //myboard.state = myboard.accept(userkey)
         switch (myboard.state) {
             case TetrisState.NewBlock:
                 var randnum = Math.floor(Math.random() * 7);//클라이언트가 랜덤넘버 생성
@@ -124,12 +125,28 @@ function App() {
                 };
                 console.log(myboard.oScreen)
                 setDrawScreen(myboard!!.oScreen.get_array()) // 렌더링 코드
-                //setMyScreen(myboard.oScreen.get_array())
+                map.set(myName,myboard)
                 stompClient!.send("/app/chat.send", {}, JSON.stringify(chatMessage))//컨트롤러의 chat.send로 매핑
                 break;
             case TetrisState.Running:
+                myboard.state = myboard.accept(userkey)
+                console.log("내보드 running 처음진입 상태 :",myboard.state)
                 console.log("key to send:", userkey)
-                var chatMessage2 = {//서버에게 보낼 메시지
+                if(myboard.state == TetrisState.NewBlock){ // 땅에 닿는 순간에는 곧장 newblock으로 바뀌기 때문에 randnum도 같이 담아서 보내준다 -> 백에서도 s키로 땅에 닿자마자 randnum를 활용하게 설계해놓음
+                    var randnum = Math.floor(Math.random() * 7);//클라이언트가 랜덤넘버 생성
+                    myboard.state = myboard.accept(randnum.toString())//클라이언트의 로직 실행
+                        var chatMessage = {//서버에게 보낼 메시지 
+                        sender: myName,//서버가 유저 판단하는 데 사용
+                        content: userkey,
+                        key: userkey,
+                        idxBT: randnum,//클라이언트가 생성한 랜덤넘버 보냄
+                    };
+                    setDrawScreen(myboard!!.oScreen.get_array()) // 렌더링 코드
+                    stompClient!.send("/app/chat.send", {}, JSON.stringify(chatMessage))//컨트롤러의 chat.send로 매핑
+                    map.set(myName,myboard)
+                    break;
+                }
+                var chatMessage2 = {//서버에게 보낼 메시지 //땅에 닿기 전까지는 그냥 key만 보내고
                     sender: myName,
                     content: userkey,
                     key: userkey,
@@ -137,12 +154,12 @@ function App() {
                 stompClient!.send("/app/chat.send", {}, JSON.stringify(chatMessage2))
                 console.log(myboard.oScreen)
                 setDrawScreen(myboard!!.oScreen.get_array()) // 렌더링 코드
-                //setMyScreen(myboard.oScreen.get_array())
+                map.set(myName,myboard)
                 break;
             case TetrisState.Finished:
                 console.log("이미 종료된 보드")
                 setDrawScreen(myboard!!.oScreen.get_array()) // 렌더링 코드
-                //setMyScreen(myboard.oScreen.get_array())
+                //setMyScreen(myboard.oScreen.get_array())s
                 break;
             default:
                 setDrawScreen(myboard!!.oScreen.get_array()) // 렌더링 코드
